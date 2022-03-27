0a0c030e03fa3636c6aa10dc26dc60e1cfdf47dc
Manraj Singh <74936295+ManrajSingh6@users.noreply.github.com>
2022-03-27 10:16:28 -0600

    Add files via upload
    You may remove the package statement at the top of every file if your files are located in a different package.

---
+++ Interface.java
@@ -0,0 +1,26 @@
+package edu.ucalgary.java;
+
+import javax.swing.*;
+
+import javax.swing.*;
+import java.awt.*;
+
+
+
+public class Interface {
+    JFrame frame = new JFrame("Sorry");
+    JLabel label = new JLabel("Sorry this area still needs to be worked on!");
+
+
+    Interface(){
+        label.setBounds(100,75,200,50);
+        frame.add(label);
+    }
+
+
+    public void setVisible(boolean b) {
+        frame.setSize(400, 150);
+        //frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
